8919d664dfb3aa7521c07b8d05faac35ad63c657
Ahdy Kefi <ahdykefi47@gmail.com>
2022-04-21 13:56:50 +0100

    Signed-off-by: Ahdy Kefi <ahdykefi47@gmail.com>

--- src/app/_services/token-storage.service.ts
+++ src/app/_services/token-storage.service.ts
@@ -19,7 +19,7 @@ export class TokenStorageService {
   public saveToken(token: string): void {
     window.sessionStorage.removeItem(TOKEN_KEY);
     window.sessionStorage.setItem(TOKEN_KEY, token);
-
+    console.log(token)
   }
   public getToken(): string | null {
     return window.sessionStorage.getItem(TOKEN_KEY);
@@ -30,7 +30,7 @@ export class TokenStorageService {
     window.sessionStorage.removeItem(USER_ROLE);
     window.sessionStorage.setItem(USER_ROLE, decodedToken.Roles)
     window.sessionStorage.setItem(USER_KEY, decodedToken.sub)
-
+    console.log("user role:::", decodedToken)
   }
   public getUser(): any {
     const user = window.sessionStorage.getItem(USER_KEY);
