4c57937c3f0ffa7c3c9be791528efecbf4c9fa6a
dalibouali <boualimohamed77@gmail.com>
2022-05-15 22:02:12 +0100

    last one

--- src/app/filter.pipe.ts
+++ src/app/filter.pipe.ts
@@ -1,22 +1,21 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { Doctor } from './doctors/DoctorModel';
 @Pipe({
-  name: 'filter',
-  pure: false
+    name: 'filter',
+    pure: false
 })
 
 
 export class FilterPipe implements PipeTransform {
     transform(doctors: Doctor[], filter: string) {
-        if (!doctors || filter==='') {
+        if (!doctors || filter === '') {
             return doctors;
         }
-   
+
         return doctors
-        .filter(doctor => 
-            {
-                
-                return doctor.username.toLowerCase().match(filter.toLowerCase()) || doctor.firstName.toLowerCase().match(filter.toLowerCase())|| doctor.lastName.toLowerCase().match(filter.toLowerCase()) //|| doctor.specialities.toUpperCase().match(filter.toUpperCase())
+            .filter(doctor => {
+
+                return doctor.username.toLowerCase().match(filter.toLowerCase()) || doctor.firstName.toLowerCase().match(filter.toLowerCase()) || doctor.lastName.toLowerCase().match(filter.toLowerCase()) || doctor.specialities.toUpperCase().match(filter.toUpperCase())
             });
     }
 }
